cc41c389090502ce1eaccb0c2ad9da8d7b3003ed
LeoGreenfor <97034848+LeoGreenfor@users.noreply.github.com>
2025-01-02 22:58:25 +0200

    set boards

--- Sudoku/Assets/Scripts/SudokuGenerator.cs
+++ Sudoku/Assets/Scripts/SudokuGenerator.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SudokuGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GenerateBoard(int cells, int rows)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
